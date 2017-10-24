bc23432a7892812a8d901a750dcb1231221b24d0
afeiship <1290657123@qq.com>
2017-10-24 14:21:09 +0800

    issue:route service

---
+++ src/components/services/route.js
@@ -0,0 +1,26 @@
+import AppBase from 'components/scripts/index';
+
+export default class {
+
+  static push(inUrl, inData) {
+    const {history} = AppBase.$.memory;
+    history && history.push({
+      pathname: inUrl,
+      state: inData
+    });
+  }
+
+  static replace(inUrl, inData) {
+    const {history} = AppBase.$.memory;
+    history && history.replace({
+      pathname: inUrl,
+      state: inData
+    });
+  }
+
+  static back() {
+    const {history} = AppBase.$.memory;
+    history && history.goBack();
+  }
+
+}

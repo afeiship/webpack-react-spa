780eeef2a812b9a8d9590cb63fe2850acc67740a
afeiship <1290657123@qq.com>
2018-07-27 16:27:13 +0800

    issue: refs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import AppBase, {$api, $config, $store} from '#';
+import AppBase, { $api, $config, $store } from '#';
 import {
   HashRouter as Router,
   Route,
@@ -12,7 +12,7 @@ import hotable from 'react-hmr-decorator';
 export default class extends AppBase {
 
   static initialState(inStore) {
-    const {login} = inStore.local;
+    const { login } = inStore.local;
     return {
       local: {
         login: login || null
@@ -25,18 +25,17 @@ export default class extends AppBase {
   }
 
   componentDidMount() {
-    const {root} = this.refs;
     AppBase.$.memory = {
-      history: root.history
+      history: this.root.history
     };
   }
 
   render() {
     return (
-      <Router ref="root">
+      <Router ref={root => this.root = root}>
         <section className="route-wrapper">
-          <Route exact path="/" component={Login}/>
-          <Route path="/modules" component={Admin}/>
+          <Route exact path="/" component={Login} />
+          <Route path="/modules" component={Admin} />
         </section>
       </Router>
     );

dd6b575fb51108612be34abc9357d049017852a4
afeiship <1290657123@qq.com>
2018-10-25 23:56:26 +0800

    issue: add pretierr

--- src/components/index.js
+++ src/components/index.js
@@ -2,16 +2,9 @@
 export default require('views/app-base').default;
 /*===default start===*/
 
-
-/*===mixins start===*/
-export const OnChangeMixin = require('mixins/on-change').default;
-/*===mixins end===*/
-
-
 /*===services start===*/
 export const $api = require('services/api').default;
 export const $config = require('services/config').default;
-export const $app = require('services/app').default;
 export const $http = require('services/http').default;
 export const $route = require('services/route').default;
 export const $store = require('next-store');
@@ -19,8 +12,6 @@ export const $date = require('next-date');
 
 /*===services end===*/
 
-
 /*===components start===*/
 export const TestComp = require('views/test-comp').default;
 /*===components end===*/
-
--- src/components/services/form.js
+++ src/components/services/form.js
@@ -1,17 +1,15 @@
-import {$config, $route} from '#';
-
+import { $config, $route } from "#";
 
 export default class {
-
   static formLayout(inArray) {
     return [
       {
         labelCol: {
-          span: inArray[0],
+          span: inArray[0]
         },
         wrapperCol: {
           span: inArray[1]
-        },
+        }
       },
       {
         wrapperCol: {
@@ -21,7 +19,4 @@ export default class {
       }
     ];
   }
-
 }
-
-

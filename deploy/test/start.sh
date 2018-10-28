9a7afc7045244a1517937900eeb74d79d9dff35f
afeiship <1290657123@qq.com>
2018-10-29 00:12:51 +0800

    issue: test build script

--- deploy/test/start.sh
+++ deploy/test/start.sh
@@ -6,11 +6,11 @@ PACKAGE_NAME='zb-activity';
 git pull
 
 ## build
-docker run -it -v $PWD:/work -w /work node /work/deploy/dev/build.sh
+docker run -it -v $PWD:/work -w /work node /work/deploy/test/build.sh
 
 ## package for prd env
 rm -rf $PACKAGE_NAME.tar.gz
-tar zcf $PACKAGE_NAME.tar.gz ./dist ./deploy/prd
+tar zcf $PACKAGE_NAME.tar.gz ./dist ./deploy/test
 
 ## reload
 docker-compose -f ./deploy/test/docker-compose.yml down

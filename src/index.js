b2b8d30bb3adf557c8601b2eb931e4ddbf94994b
afeiship <afeiship@gmail.com>
2018-12-26 01:33:53 +0800

    feat: add appRender decorator instead off app.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+import AppBase, { $api, $config, $store } from '#';
+import { HashRouter as Router, Switch } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { appRender } from 'next-react-redux';
+import hotable from 'react-hmr-decorator';
+import routes from './routes';
 import 'assets/styles/index.scss';
-import { ReduxBoot } from 'next-react-redux';
-import Loadable from 'react-loadable';
-import App from './app';
 
-// loadable:
-Loadable.preloadReady().then(() => {
-  ReduxBoot.run(App, 'root', {
-    prefix: 'react-spa'
-  });
-});
+@appRender('root', { prefix: 'react-spax' })
+@hotable(module)
+export default class extends AppBase {
+  static initialState(inStore) {
+    const { login } = inStore.local;
+    return {
+      local: {
+        login: login || null
+      },
+      memory: {
+        modalUser: false,
+        modalUserQuery: false
+      }
+    };
+  }
+
+  componentDidMount() {
+    AppBase.$.memory = {
+      history: this.root.history
+    };
+  }
+
+  render() {
+    return (
+      <Router ref={(root) => (this.root = root)}>
+        <Switch>{renderRoutes(routes)}</Switch>
+      </Router>
+    );
+  }
+}

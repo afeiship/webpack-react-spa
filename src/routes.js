e9c1ae7ef3c26f5d2be07b7f8587f3b08c6262d0
afeiship <afeiship@gmail.com>
2018-12-25 23:51:39 +0800

    feat: basic dynamic import

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,30 @@
-import Login from './login';
-import Admin from './admin';
+import Loadable from 'react-loadable';
+
+// https://juejin.im/post/5a582956518825734216d282
+const LoadingComponent = ({ isLoading, error }) => {
+  if (isLoading) {
+    return <div className="f20 webkit-sassui-transform-center-xy">Loading...</div>;
+  } else if (error) {
+    return <div>Sorry, there was a problem loading the page.</div>;
+  } else {
+    return null;
+  }
+};
 
 export default [
   {
     path: '/',
     exact: true,
-    component: Login
+    component: Loadable({
+      loader: () => import('./login'),
+      loading: LoadingComponent
+    })
   },
   {
     path: '/admin',
-    component: Admin
+    component: Loadable({
+      loader: () => import('./admin'),
+      loading: LoadingComponent
+    })
   }
 ];

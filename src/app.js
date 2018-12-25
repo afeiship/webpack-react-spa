50a8fed184a0721739c1154a1bfea3c60bfd622e
afeiship <afeiship@gmail.com>
2018-12-25 08:16:29 +0800

    add: eslint

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
-import AppBase, {$api, $config, $store} from '#';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import AppBase, { $api, $config, $store } from '#'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
-import Login from './login';
-import Admin from './admin';
-import hotable from 'react-hmr-decorator';
+import Login from './login'
+import Admin from './admin'
+import hotable from 'react-hmr-decorator'
 
 @hotable(module)
 export default class extends AppBase {
-  static initialState(inStore) {
-    const {login} = inStore.local;
-    return {
-      local: {
-        login: login || null
-      },
-      memory: {
-        modalUser: false,
-        modalUserQuery: false
-      }
-    };
-  }
+	static initialState(inStore) {
+		const { login } = inStore.local
+		return {
+			local: {
+				login: login || null
+			},
+			memory: {
+				modalUser: false,
+				modalUserQuery: false
+			}
+		}
+	}
 
-  componentDidMount() {
-    AppBase.$.memory = {
-      history: this.root.history
-    };
-  }
+	componentDidMount() {
+		AppBase.$.memory = {
+			history: this.root.history
+		}
+	}
 
-  render() {
-    return (
-      <Router ref={(root) => (this.root = root)}>
-        <section className="route-wrapper">
-          <Route exact path="/" component={Login}/>
-          <Route path="/modules" component={Admin}/>
-        </section>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router ref={(root) => (this.root = root)}>
+				<section className="route-wrapper">
+					<Route exact path="/" component={Login} />
+					<Route path="/modules" component={Admin} />
+				</section>
+			</Router>
+		)
+	}
 }

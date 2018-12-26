79c3b7af6a80726b1ccc675bcc0497ca2f7d9585
afeiship <afeiship@gmail.com>
2018-12-27 00:57:28 +0800

    feat: remove import scss

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import AppBase, { $api, $config, $store } from '#';
 import { HashRouter as Router, Switch } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
+// import { appRender } from 'next-react-redux';
 import hotable from 'react-hmr-decorator';
 import routes from './routes';
-import 'assets/styles/index.scss';
 
+// @appRender('root', { prefix: 'react-spa' })
 @hotable(module)
 export default class extends AppBase {
   static initialState(inStore) {

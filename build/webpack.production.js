6ffafb93b51d3a6899f66aebb2b7163bbc6ed1ec
afeiship <1290657123@qq.com>
2019-03-30 23:46:07 +0800

    add: archiver plugin

--- build/webpack.production.js
+++ build/webpack.production.js
@@ -1,6 +1,9 @@
 import merge from 'webpack-merge';
 import base from './base';
+import { plugins } from 'webpack-app-kits';
 
 export default (inEnv) => {
-  return merge(base(inEnv));
+  return merge(base(inEnv), {
+    plugins: [plugins.archiver()]
+  });
 };

276f03b8f2012502085246bf197545560f2f4bf3
afeiship <1290657123@qq.com>
2019-05-13 23:21:02 +0800

    add: config offline for prd

--- build/webpack.production.js
+++ build/webpack.production.js
@@ -1,18 +1,12 @@
 import merge from 'webpack-merge';
 import base from './base';
+import config from './config';
 import { plugins } from 'webpack-app-kits';
 
 export default (inEnv) => {
   return merge(base(inEnv), {
     plugins: [
-      plugins.offline({
-        externals: [
-          '//cdn.bootcss.com/spinkit/1.2.5/spinkit.min.css',
-          '//cdn.bootcss.com/antd/3.10.1/antd.min.css',
-          '//cdn.bootcss.com/react/16.4.0/umd/react.production.min.js',
-          '//cdn.bootcss.com/react-dom/16.4.0/umd/react-dom.production.min.js'
-        ]
-      })
+      plugins.offline( config.offline )
     ]
   });
 };

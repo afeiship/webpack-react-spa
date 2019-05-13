34ae40434dac56964c58998ea14f5b134e1cef2a
afeiship <1290657123@qq.com>
2019-05-13 23:22:57 +0800

    fix: dev

--- build/webpack.development.js
+++ build/webpack.development.js
@@ -1,12 +1,10 @@
 import merge from 'webpack-merge';
 import config from './config';
-import { plugins, configs } from 'webpack-app-kits';
-
 import base from './base';
+import { plugins, configs } from 'webpack-app-kits';
 
 export default (inEnv) => {
   const { port, proxy } = config;
-
   return merge(base(inEnv), {
     devtool: configs.devtool(),
     plugins: [plugins.hotModuleReplacement()],

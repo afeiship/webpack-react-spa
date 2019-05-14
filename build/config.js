4673225942ea94071ca92e40f5d54ee33de9d8ec
afeiship <1290657123@qq.com>
2019-05-14 09:37:16 +0800

    add: optmize config

--- build/config.js
+++ build/config.js
@@ -1,3 +1,12 @@
+const nx = require('next-js-core2');
+const nxValues = require('next-values');
+const libs = {
+  spinKit: '//cdnjs.cloudflare.com/ajax/libs/spinkit/1.2.5/spinkit.min.css',
+  antdCss: '//cdnjs.cloudflare.com/ajax/libs/antd/3.12.1/antd.min.css',
+  react: '//cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js',
+  reactDom: '//cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.4/umd/react-dom.production.min.js'
+};
+
 module.exports = {
   port: '3005',
   proxy: {
@@ -8,38 +17,22 @@ module.exports = {
   },
   vendors: ['classnames', 'react-router', 'react-router-dom'],
   offline: {
-    externals: [
-      '//cdnjs.cloudflare.com/ajax/libs/spinkit/1.2.5/spinkit.min.css',
-      '//cdnjs.cloudflare.com/ajax/libs/antd/3.12.1/antd.min.css',
-      '//cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js',
-      '//cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.4/umd/react-dom.production.min.js'
-    ]
+    externals: nxValues(libs)
   },
   development: {
     publicPath: '/',
-    libs: {
-      spinKit: '//cdnjs.cloudflare.com/ajax/libs/spinkit/1.2.5/spinkit.min.css',
+    libs: nx.mix(null, libs, {
       antdCss: '/antd/dist/antd.css',
       react: '/react/umd/react.development.js',
       reactDom: '/react-dom/umd/react-dom.development.js'
-    }
+    })
   },
   test: {
     publicPath: '/app/',
-    libs: {
-      spinKit: '//cdnjs.cloudflare.com/ajax/libs/spinkit/1.2.5/spinkit.min.css',
-      antdCss: '//cdnjs.cloudflare.com/ajax/libs/antd/3.12.1/antd.min.css',
-      react: '//cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js',
-      reactDom: '//cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.4/umd/react-dom.production.min.js'
-    }
+    libs: libs
   },
   production: {
     publicPath: '/',
-    libs: {
-      spinKit: '//cdnjs.cloudflare.com/ajax/libs/spinkit/1.2.5/spinkit.min.css',
-      antdCss: '//cdnjs.cloudflare.com/ajax/libs/antd/3.12.1/antd.min.css',
-      react: '//cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js',
-      reactDom: '//cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.4/umd/react-dom.production.min.js'
-    }
+    libs: libs
   }
 };

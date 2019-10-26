44856959463dc0024d83ad966054c0983365a3c7
afeiship <1290657123@qq.com>
2019-10-26 14:39:43 +0800

    add: brwoserrc

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: [
     require('autoprefixer')({
-      browsers: [
-        'last 2 version',
-        'Explorer >= 10',
-        'Android >= 4'
-      ],
       remove: false
     })
   ]

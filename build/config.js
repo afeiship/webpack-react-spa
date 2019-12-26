9be354281bbbe8d7628076a19c09a64b44022ebc
afeiship <1290657123@qq.com>
2019-12-26 15:56:56 +0800

    add: loading-screen

--- build/config.js
+++ build/config.js
@@ -1,14 +1,15 @@
 const nx = require('@feizheng/next-js-core2');
 const nxValues = require('@feizheng/next-values');
 const libs = {
-  spinKit: '//cdnjs.cloudflare.com/ajax/libs/spinkit/1.2.5/spinkit.min.css',
+  nextJsCore2: '//unpkg.com/@feizheng/next-js-core2@2.0.5/dist/next-js-core2.min.js',
+  loadingScreen: '//unpkg.com/@feizheng/loading-screen@1.0.2/dist/style.css',
   antdCss: '//cdnjs.cloudflare.com/ajax/libs/antd/3.12.1/antd.min.css',
   react: '//cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js',
   reactDom: '//cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.4/umd/react-dom.production.min.js'
 };
 
 module.exports = {
-  port: '3005',
+  port: '3006',
   proxy: {
     '/api': {
       target: 'http://120.27.13.225:80',

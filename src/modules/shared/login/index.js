ba0b3529775e4f677b662e6e4cfef0c8b4c412de
afeiship <1290657123@qq.com>
2020-10-04 17:56:37 +0800

    feat: add antd-schema

--- src/modules/shared/login/index.js
+++ src/modules/shared/login/index.js
@@ -1,17 +1,8 @@
-import ReactFullImage from '@feizheng/react-full-image';
 import bgImg from '@/assets/images/bg.jpg';
+import ReactFullImage from '@feizheng/react-full-image';
 import React, { Component } from 'react';
-import ApiServie from '@/services/api';
-import { Form, Input, Button, Checkbox } from 'antd';
+import Form from './form';
 import './style.scss';
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
 
 @service(['route', 'api'])
 @mixin(['on-change'])
@@ -37,38 +28,7 @@ export default class extends Component {
       <div className="h100 rel">
         <ReactFullImage src={bgImg} />
         <div className="p20 bg-f shadow-5 abs webkit-sassui-transform-center-xy login-view">
-          <Form
-            {...layout}
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={this.onFinish}
-          >
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-              <Input.Password />
-            </Form.Item>
-
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item {...tailLayout}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+          <Form />
         </div>
       </div>
     );

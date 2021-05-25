b556d2580a283df6ea280d115f1cee11675b48a0
afeiship <1290657123@qq.com>
2021-05-25 20:17:16 +0800

    feat: styled-box support

--- src/modules/shared/login/index.js
+++ src/modules/shared/login/index.js
@@ -3,6 +3,7 @@ import ReactFullImage from '@jswork/react-full-image';
 import { Button, Form } from 'antd';
 import FormBuilder from 'antd-form-builder';
 import React, { Component } from 'react';
+import StyledBox from '@jswork/styled-box';
 
 const Container = styled.div`
   width: 400px;
@@ -40,10 +41,12 @@ export default class extends Component {
       <div className="h100 rel">
         <ReactFullImage src={bgImg} />
         <Container className="p-20 bg-f shadow-5 abs wsui-transform-center-xy">
-          <nx.$rc.layout value="la" className="mb-10">
-            <div className="bg-e p-10">left</div>
-            <div className="bg-8 p-10">right</div>
-          </nx.$rc.layout>
+          <StyledBox mb={20} c={'red'} px={10}>
+            <nx.$rc.layout value="la" className="mb-10">
+              <div className="bg-e p-10">left</div>
+              <div className="bg-8 p-10">right</div>
+            </nx.$rc.layout>
+          </StyledBox>
           <Form onFinish={this.handleFinish}>
             <FormBuilder meta={meta} />
             <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
